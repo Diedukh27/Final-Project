0ec4642cf78481b4ef66125709655acc836f9067
school <school@A42PC11>
2025-03-11 14:31:52 +0200

    3 lesson

--- 1 lesson/Program.cs
+++ 1 lesson/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр мови
 Console.Clear(); // очищення консолі
-
-Console.WriteLine("------------- Welcome to Pizzaira! -------------");
+Pizza lll = new Pizza();
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("------------- Welcome to Pizzaira! -------------");
 
 Console.WriteLine("\tADMIN MENU\n" +
                   "1. Add New Pizza\n" +
@@ -19,16 +22,25 @@ Console.WriteLine("______________________________");
 Console.Write("your choice:");
 int choice = int.Parse(Console.ReadLine());
 
+
 switch (choice)
 {
     case 1:
-        var item = new Pizza();
-        item.Name = Console.ReadLine();
-        item.Description = Console.ReadLine();
-        item.Price = double.Parse(Console.ReadLine());
-        item.size = double.Parse(Console.ReadLine());
-        break;  
+        lll.AddPizza();
+        break;
+
+
+        case 7:
+        lll.Show();     
+            break;
+
+
 }
+
+    Console.ReadKey();
+
+}
+
 
 public class Pizza
 {
@@ -37,8 +49,42 @@ public class Pizza
     public string Description { get; set; }
     public double Price { get; set; }
     public double size { get; set;}
-    
+
+    public void AddPizza()
+    {
+        Console.Write("Enter name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter Description: ");
+        Description = Console.ReadLine();
+        Console.Write("Enter Price: ");
+        Price = double.Parse(Console.ReadLine());
+        Console.Write("Enter size: ");
+        size = double.Parse(Console.ReadLine());
+    }
+    public void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Description: {Description}$");
+        Console.WriteLine($"Price: {Price}$");
+        Console.WriteLine($"size: {size}%");
+        
+    }
+
+
 }
+
+
+public class CustomPizza : Pizza
+{
+    public string NameClient { get; set; }
+
+ 
+}
+
+
+
+
+
 
 
 

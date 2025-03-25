b44d543d71be7b527b6cf68d20ac66fb3f1dae58
Maks Diedukh <maks.diedukh4@gmail.com>
2025-03-25 14:05:12 +0200

    project

--- 1 lesson/Program.cs
+++ 1 lesson/Program.cs
@@ -1,22 +1,15 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр мови
+﻿
+
+using System.Text.Json;
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр мови
 Console.Clear(); // очищення консолі
 List<Pizza> items = new()
 {
     new Pizza() { Name = "Margarita", Description = "Chesse, Tomato", Price = 12, size = 30},
     new Pizza() { Name = "Myasna", Description = "Shynka, Salyami ", Price = 20, size = 40 }
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 List<CustomPizza> itemss = new(); 
 
@@ -53,6 +46,10 @@ int choice = int.Parse(Console.ReadLine());
 
 switch (choice)
 {
+         case 0: Console.WriteLine("Have a good day");
+            return 0;
+
+
     case 1:
             Pizza newItem = new();
             newItem.AddPizza();
@@ -60,10 +57,44 @@ switch (choice)
             break;
 
     case 2:
+            Console.Write("Enter Pizza name to search: ");
+            string name2 = Console.ReadLine();
+
+            // логіка пошуку продукта
+            var found2 = items.Find(x => x.Name == name2);
+            if (found2 == null)
+            {
+                Console.WriteLine("Pizza not found!");
+                break;
+            }
+
+            items.Remove(found2);
+            break;
+
+        case 22:
             CustomPizza newItems = new();
             newItems.AddPizza();
             itemss.Add(newItems);
             break;
+
+        case 5:
+            var json = JsonSerializer.Serialize(items);
+            File.WriteAllText("database.json", json);
+
+            var json2 = JsonSerializer.Serialize(itemss);
+            File.WriteAllText("database.json", json);
+            break;
+
+        case 52:
+            var jsonData = File.ReadAllText("database.json");
+            items = JsonSerializer.Deserialize<List<Pizza>>(jsonData);
+
+            var jsonData2 = File.ReadAllText("CustomPizza.json");
+            itemss = JsonSerializer.Deserialize<List<CustomPizza>>(jsonData2);
+            break;
+
+
+
 
 
     case 7:
@@ -71,30 +102,43 @@ switch (choice)
                 item.Show();
             break;
 
-    case 8:
+    case 72:
             foreach (CustomPizza item in itemss)
                 item.Show();
             break;
 
-   case 9:
-            Console.Write("Enter product name to search: ");
+   case 8:
+            Console.Write("Enter Pizza name to search: ");
             string name = Console.ReadLine();
 
             // логіка пошуку продукта
             var found = items.Find(x => x.Name == name);
             if (found == null)
             {
-                Console.WriteLine("Product not found!");
+                Console.WriteLine("Pizza not found!");
                 break;
             }
 
             found.Show();
             break;
 
+            case 10:
+            Console.Write("Enter Pizza name to buy: ");
+            string name3 = Console.ReadLine();
 
+            var prodToBuy = items.Find(x => x.Name == name3);
+            if (prodToBuy == null)
+            {
+                Console.WriteLine("Pizza not found!");
+                break;
+            }
 
-
+            Console.WriteLine("Your pizza will be ready in 20 minutes.");
+            break;  
     }
+
+
+
     Console.WriteLine("Натистінь щось для продовження...");
     Console.ReadKey();
 

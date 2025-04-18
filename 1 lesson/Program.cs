34981999bcdd1fd7a23bff364004d367faf61121
school <school@A42PC11>
2025-04-18 13:34:26 +0300

    spfsf

--- 1 lesson/Program.cs
+++ 1 lesson/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using _1_lesson;
 using System.Text.Json;
 
 
@@ -22,68 +23,66 @@ while (true)
     Console.Clear();
     Console.WriteLine("------------- Welcome to Pizzaira! -------------");
 
-Console.WriteLine("\tADMIN MENU\n" +
-                  "1. Add New Pizza\n" +
-                  "2. Delete Pizza\n" +
-                  "3. Add Pizza Product\n" +
-                  "4. Detele Product Pizza\n" +
-                  "5. Збереження бази піц у файлі\n" +
-                  "6. Завантаження бази піц з файлу\n" +
-                  "7. Перегляд  готових піц(складників, ціни)\n" +
-                  "8. Пошук піц по складникам\n" +
-                  "9. Замовлення піци клієнтом(ами), враховуючи можливе складання піци клієнтом\n" +
-                  "10. Розрахунок клієнта(друк чеку, прийом оплати)");
-Console.WriteLine("______________________________");
+    Console.WriteLine("\tADMIN MENU\n" +
+                      "1. Add New Pizza\n" +
+                      "2. Delete Pizza\n" +
+                      "3. Add Pizza Product\n" +
+                      "4. Detele Product Pizza\n" +
+                      "5. Збереження бази піц у файлі\n" +
+                      "6. Завантаження бази піц з файлу\n" +
+                      "7. Перегляд  готових піц(складників, ціни)\n" +
+                      "8. Пошук піц по складникам\n" +
+                      "9. Замовлення піци клієнтом(ами), враховуючи можливе складання піци клієнтом\n" +
+                      "10. Розрахунок клієнта(друк чеку, прийом оплати)");
+    Console.WriteLine("______________________________");
 
-Console.Write("your choice:");
-int choice = int.Parse(Console.ReadLine());
+    Console.Write("your choice:");
+    int choice = int.Parse(Console.ReadLine());
 
+    var p = new Pizzaria();
 
-switch (choice)
-{
-         case 0: Console.WriteLine("Have a good day");
+    switch (choice)
+    {
+        case 0:
+            Console.WriteLine("Have a good day");
             return 0;
 
 
-    case 1:
-            
+        case 1:
+            p.AddNewPizza();
             break;
 
-    case 2:
-            
+        case 2:
+            p.RemoveNewPizza();
             break;
 
         case 22:
-           
+            p.AddNewCustomPizza();
             break;
 
         case 5:
-            
+            p.SavePizza();
             break;
 
         case 52:
-           
+            p.LoadPizzaInFail();
             break;
 
-
-
-
-
-    case 7:
-            
+        case 7:
+            p.ForeachPizza();
             break;
 
-    case 72:
-            
+        case 72:
+            p.ForeachCustomPizza();
             break;
 
-   case 8:
-           
+        case 8:
+            p.FindPizza();
             break;
 
-            case 10:
-           
-            break;  
+        case 10:
+            p.BuyPizza();
+            break;
     }
 
 
@@ -92,44 +91,6 @@ switch (choice)
     Console.ReadKey();
 
 }
-
-
-public class CustomPizza : Pizza
-{
-    public string NameClientt { get; set; }
-
-    public void AddPizza()
-    {
-        Console.Write("Enter name: ");
-        Name = Console.ReadLine();
-        Console.Write("Enter Description: ");
-        Description = Console.ReadLine();
-        Console.Write("Enter Price: ");
-        Price = double.Parse(Console.ReadLine());
-        Console.Write("Enter size: ");
-        size = double.Parse(Console.ReadLine());
-        Console.Write("Enter NameClient: ");
-        NameClientt = (Console.ReadLine());
-
-    }
-    public void Show()
-
-    {
-        Console.WriteLine("------ Pizza ------");
-        Console.WriteLine($"Name: {Name}");
-        Console.WriteLine($"Description: {Description}$");
-        Console.WriteLine($"Price: {Price}$");
-        Console.WriteLine($"size: {size}%");
-        Console.WriteLine($"NameClient: {NameClientt}%");
-
-    }
-
-
-}
-
-
-
-
 
 
 

40e7b8055e75e608a941fbf0af1ecdcb35e31786
school <school@A42PC11>
2025-04-25 13:58:46 +0300

    Final Project is finish

---
+++ 1 lesson/Menu.cs
@@ -0,0 +1,41 @@
+﻿
+public class Menu
+{
+    public void ShowAdminMenu()
+    {
+        Console.WriteLine("\tADMIN MENU\n" +
+                          "1. Add New Pizza\n" +
+                      "2. Delete Pizza\n" +
+                      "3. Add Pizza Product\n" +
+                      "4. Detele Product Pizza\n" +
+                      "5. Збереження бази піц у файлі\n" +
+                      "6. Завантаження бази піц з файлу\n" +
+                      "7. Перегляд  готових піц(складників, ціни)\n" +
+                      "8. Пошук піц по складникам\n" +
+                      "9. Замовлення піци клієнтом(ами), враховуючи можливе складання піци клієнтом\n" +
+                      "10. Розрахунок клієнта(друк чеку, прийом оплати)");
+        Console.WriteLine("______________________________");
+    }
+    public void ShowUserMenu()
+    {
+        Console.WriteLine("\tUSER MENU\n" +
+                          "0. Exit\n" +
+                          "1. Order Pizza\n" +
+                          "2. Order Custom Pizza");
+        Console.WriteLine("______________________________");
+    }
+    public void ShowLoginMenu()
+    {
+        Console.WriteLine("\tCHOOSE YOUR ROLE\n" +
+                          "0. Exit\n" +
+                          "1. User\n" +
+                          "2. Admin\n");
+        Console.WriteLine("______________________________");
+    }
+
+    public int AskUserInput()
+    {
+        Console.Write("Your choice: ");
+        return int.Parse(Console.ReadLine());
+    }
+}
--- 1 lesson/Program.cs
+++ 1 lesson/Program.cs
@@ -1,96 +1,103 @@
-﻿
-
-using _1_lesson;
+﻿using _1_lesson;
 using System.Text.Json;
-
 
 Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр мови
 Console.Clear(); // очищення консолі
 
 
+Menu menu = new ();
+Pizzaria p = new ();
 
+menu.ShowLoginMenu();
+int roleChoice = menu.AskUserInput();
 
-
-
-
-
-
-
-
-
-while (true)
+if (roleChoice == 1)
 {
-    Console.Clear();
-    Console.WriteLine("------------- Welcome to Pizzaira! -------------");
+    Console.Clear(); // очищення консолі
+    Console.WriteLine("------------- Welcome to Pizzaria for User! -------------");
 
-    Console.WriteLine("\tADMIN MENU\n" +
-                      "1. Add New Pizza\n" +
-                      "2. Delete Pizza\n" +
-                      "3. Add Pizza Product\n" +
-                      "4. Detele Product Pizza\n" +
-                      "5. Збереження бази піц у файлі\n" +
-                      "6. Завантаження бази піц з файлу\n" +
-                      "7. Перегляд  готових піц(складників, ціни)\n" +
-                      "8. Пошук піц по складникам\n" +
-                      "9. Замовлення піци клієнтом(ами), враховуючи можливе складання піци клієнтом\n" +
-                      "10. Розрахунок клієнта(друк чеку, прийом оплати)");
-    Console.WriteLine("______________________________");
+    menu.ShowUserMenu();
+    int choice = menu.AskUserInput();
 
-    Console.Write("your choice:");
-    int choice = int.Parse(Console.ReadLine());
-
-    var p = new Pizzaria();
+    p.LoadPizzaInFail();
 
     switch (choice)
     {
-        case 0:
-            Console.WriteLine("Have a good day");
-            return 0;
-
-
+        case 0: return 0;
         case 1:
-            p.AddNewPizza();
-            break;
-
-        case 2:
-            p.RemoveNewPizza();
-            break;
-
-        case 22:
-            p.AddNewCustomPizza();
-            break;
-
-        case 5:
-            p.SavePizza();
-            break;
-
-        case 52:
-            p.LoadPizzaInFail();
-            break;
-
-        case 7:
+            Console.WriteLine("You can buy something!");
             p.ForeachPizza();
-            break;
-
-        case 72:
             p.ForeachCustomPizza();
-            break;
-
-        case 8:
-            p.FindPizza();
-            break;
-
-        case 10:
             p.BuyPizza();
             break;
     }
-
-
-
-    Console.WriteLine("Натистінь щось для продовження...");
-    Console.ReadKey();
-
 }
+
+
+if (roleChoice == 2)
+{
+    while (true)
+    {
+        Console.Clear(); // очищення консолі
+        Console.WriteLine("------------- Welcome to Supermarket for Admin! -------------");
+
+        menu.ShowAdminMenu();
+        int choice = menu.AskUserInput();
+
+        switch (choice)
+        {
+            case 0:
+                Console.WriteLine("Have a good day");
+                return 0;
+
+
+            case 1:
+                p.AddNewPizza();
+                break;
+
+            case 2:
+                p.RemoveNewPizza();
+                break;
+
+            case 22:
+                p.AddNewCustomPizza();
+                break;
+
+            case 5:
+                p.SavePizza();
+                break;
+
+            case 52:
+                p.LoadPizzaInFail();
+                break;
+
+            case 7:
+                p.ForeachPizza();
+                break;
+
+            case 72:
+                p.ForeachCustomPizza();
+                break;
+
+            case 8:
+                p.FindPizza();
+                break;
+
+            case 10:
+                p.BuyPizza();
+                break;
+        }
+
+        Console.WriteLine("Натистінь щось для продовження...");
+        Console.ReadKey();
+    }
+}
+
+Console.WriteLine("Гарного дня!");
+return 0;
+
+
+
 
 
 
